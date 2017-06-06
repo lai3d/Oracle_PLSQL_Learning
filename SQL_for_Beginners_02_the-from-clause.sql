62b2e275319287dbab2e0609a031aaa71103d413
Larry Lai <yanlin.lai@afusion.com>
2017-06-06 17:32:55 +0800

    Keep learning

--- SQL_for_Beginners_02_the-from-clause.sql
+++ SQL_for_Beginners_02_the-from-clause.sql
@@ -1,3 +1,70 @@
+-- https://oracle-base.com/articles/misc/sql-for-beginners-the-from-clause
+
+-------------------------------------- Setup ------------------------------------------
+--------------------------------------  ------------------------------------------
+
+SELECT e.employee_name, d.department_name
+FROM   employees e
+       JOIN departments d ON d.department_id = e.department_id
+ORDER BY e.employee_name;
+
+-- non-ANSI syntax for joins
+
+SELECT e.employee_name, d.department_name
+FROM   employees e, departments d
+WHERE  d.department_id = e.department_id
+ORDER BY e.employee_name;
+
+-------------------------------------- Inline Views ------------------------------------------
+
+SELECT ed.employee_name, ed.department_name
+FROM   (SELECT e.employee_name, d.department_name
+        FROM   employees e, departments d
+        WHERE  d.department_id = e.department_id) ed
+ORDER BY ed.employee_name;
+
+-------------------------------------- WITH Clause ------------------------------------------
+
+WITH emp_dept_join AS (
+  SELECT e.employee_name, d.department_name
+  FROM   employees e, departments d
+  WHERE  d.department_id = e.department_id
+)
+SELECT ed.employee_name, ed.department_name
+FROM   emp_dept_join ed
+ORDER BY ed.employee_name;
+
+-------------------------------------- Views ------------------------------------------
+
+CREATE OR REPLACE VIEW emp_dept_join_v AS
+  SELECT e.employee_name, d.department_name
+  FROM   employees e, departments d
+  WHERE  d.department_id = e.department_id;
+
+SELECT ed.employee_name, ed.department_name
+FROM   emp_dept_join_v ed
+ORDER BY ed.employee_name;
+
+-------------------------------------- Pipelined Table Functions ------------------------------------------
+
+CREATE TYPE t_employee_name_tab AS TABLE OF VARCHAR2(10);
+/
+
+CREATE OR REPLACE FUNCTION get_employee_names
+  RETURN t_employee_name_tab PIPELINED
+AS
+BEGIN
+  FOR cur_rec IN (SELECT employee_name FROM employees) LOOP
+    PIPE ROW (cur_rec.employee_name);
+  END LOOP;
+  RETURN;
+END;
+/
+
+SELECT e.column_value AS employee_name
+FROM   TABLE(get_employee_names) e
+ORDER BY e.column_value;
+
 
 --------------------------------------  ------------------------------------------
 
---
+++ SQL_for_Beginners_03_the-where-clause.sql
@@ -0,0 +1,155 @@
+-- https://oracle-base.com/articles/misc/sql-for-beginners-the-where-clause
+
+-------------------------------------- Introduction ------------------------------------------
+
+-- non-ANSI join syntax
+
+SELECT e.employee_name, e.salary, d.department_id, d.department_name
+FROM   employees e, departments d
+WHERE  e.department_id = d.department_id  -- Join Condition
+AND    d.department_id = 20               -- Filter
+AND    e.salary >= 2000                   -- Filter
+ORDER BY e.employee_name;
+
+-- ANSI join syntax
+
+SELECT e.employee_name, e.salary, d.department_id, d.department_name
+FROM   employees e
+       JOIN departments d ON e.department_id = d.department_id
+WHERE  d.department_id = 20               -- Filter
+AND    e.salary >= 2000                   -- Filter
+ORDER BY e.employee_name;
+
+-------------------------------------- Equality and Inequality Conditions ------------------------------------------
+/*
+= : Equal.
+<> or != : Not equal.
+> : Greater than.
+< : Less than.
+>= : Great than or equal.
+<= : Less than or equal.
+*/
+
+SELECT e.employee_name, e.department_id, e.salary
+FROM   employees e
+WHERE  e.department_id = 20
+AND    e.salary >= 2000
+ORDER BY e.employee_name;
+
+-------------------------------------- IN and NOT IN Conditions ------------------------------------------
+
+SELECT e.department_id, e.employee_id, e.employee_name
+FROM   employees e
+WHERE  e.department_id IN (10, 20)
+ORDER BY e.department_id, e.employee_id;
+
+SELECT e.department_id, e.employee_id, e.employee_name
+FROM   employees e
+WHERE  e.department_id NOT IN (10, 20)
+ORDER BY e.department_id, e.employee_id;
+
+SELECT e.department_id, e.employee_id, e.employee_name
+FROM   employees e
+WHERE  e.department_id IN (SELECT d.department_id
+                           FROM   departments d
+                           WHERE  d.department_id < 30)
+ORDER BY e.department_id, e.employee_id;
+
+-------------------------------------- EXISTS and NOT EXISTS Conditions ------------------------------------------
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  EXISTS (SELECT 1
+               FROM   employees e
+               WHERE  d.department_id = e.department_id)
+ORDER BY d.department_id;
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  NOT EXISTS (SELECT 1
+                   FROM   employees e
+                   WHERE  d.department_id = e.department_id)
+ORDER BY d.department_id;
+
+-------------------------------------- BETWEEN and NOT BETWEEN Conditions ------------------------------------------
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  department_id BETWEEN 20 AND 40 -- within the specified range, boundaries inclusive
+ORDER BY d.department_id;
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  department_id NOT BETWEEN 20 AND 40
+ORDER BY d.department_id;
+
+-------------------------------------- LIKE and NOT LIKE Conditions ------------------------------------------
+
+SELECT d.department_id, d.department_name
+FROM   departments d
+WHERE  department_name LIKE '%O%'
+ORDER BY d.department_id;
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
+
+
+--------------------------------------  ------------------------------------------
